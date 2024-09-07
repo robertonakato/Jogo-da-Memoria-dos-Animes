437b7222d273bbfa6a83a3d2f08a4095806b4341
robertonakato <132300963+robertonakato@users.noreply.github.com>
2024-09-06 21:53:37 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ let explicar1 = 0;
 
 
 const seguinte =[
-    "<p>Olá claro Jogador, Eu me chamo Ray e vou te explicar As regras:</p>",
+    "<p>Olá claro Jogador, Eu me chamo gojo e vou te explicar As regras:</p>",
     "<p>1 - Como qualquer jogo da memoria, segue as regras padrão de todos, ache duas figuras iguais para marcar seus pontos</p>",
     "<p> 2 - Ache três Pares Iguais em sequência para ganhar pontos extras</p></br>",
     "<p> 3 - Erre três vezes em sequência e perca um ponto</p></br>",
